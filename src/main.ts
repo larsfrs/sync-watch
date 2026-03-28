0ec25de8a91e2dbfaad39d70bdb9142d41e6fa5c
larsfrs <217333895+larsfrs@users.noreply.github.com>
2026-03-28 16:36:15 +0100

    Remove boilerplate code

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
+import 'bootstrap/js/dist/dropdown.js';
+import 'bootstrap/js/dist/collapse.js';
+import 'bootstrap/js/dist/tooltip.js';
+
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { invoke } from "@tauri-apps/api/core";
-
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
-
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
